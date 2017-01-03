3bf1ed797fec91e766255c117f7a666352a30992
Dimitar Gueorguiev <dimitar_pg13@hotmail.com>
2017-01-03 05:26:40 -0500

    training data in csv format parsing..

--- logistic_regression.py
+++ logistic_regression.py
@@ -3,35 +3,71 @@ import os
 
 
 # initialize variables/model parameters
-W = tf.Variable(tf.zeros([2,1]),name="weights")
+W = tf.Variable(tf.zeros([5,1]),name="weights")
 b = tf.Variable(0., name="bias")
 
 # define the training loop operations
-def inference(X):
+
+# former inference is now used for combining inputs
+def combine_inputs(X):
     return tf.matmul(X, W) + b
+
+# new inferred value is the signmoid applied to the former
+def inference(X):
+    return tf.sigmoid(combine_inputs(X)) 
   # compute inference model over data X and return the result
 
 def loss(X,Y):
   # compute loss over training data X and expected outputs Y
-    Y_predicted = inference(X)
-    return tf.reduce_sum(tf.squared_difference(Y, Y_predicted))
+    #Y_predicted = inference(X)
+    #return tf.reduce_sum(tf.squared_difference(Y, Y_predicted))
+    return tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(combine_inputs(X), Y))
+
+def read_csv(batch_size, file_name, record_defaults):
+    filename_queue = tf.train.string_input_producer([os.path.dirname(__filename__) + "/" + file_name])
+
+    reader = tf.TextLineReader(skip_header_lines=1)
+
+    key, value = reader.read(filename_queue)
+
+    # decode_csv will convert a tensorfrom type string (the text line) in a
+    # tuple of tensor columns with the specified defaults, which also 
+    # sets the data type for each column 
+
+    decoded =  tf.decode_csv(value, record_defaults=record_defaults)
+    
+    # batch actually reads the file and loads "batch_size" rows in a single tensor
+    
+    return tf.train.shuffle_batch(decoded, batch_size=batch_size,
+                                  capacity=batch_size * 50,
+                                  min_after_dequeue=batch_size)
 
 def inputs():
-  # read/generate input training data X and expected outputs Y
-    weight_age = [[84, 46], [73, 20], [65, 52], [70, 30],
-                  [76, 57], [69, 25], [63, 28], [72, 36],
-                  [79, 57], [75, 44], [27, 24], [89, 31],
-                  [65, 52], [57, 23], [59, 60], [69, 48],
-                  [60, 34], [79, 51], [75, 50], [82, 34],
-                  [59, 46], [67, 23], [85, 37], [55, 40],
-                  [63, 30]]
+    passenger_id, survived, pclass, name, sex, age, sibsp, parch, ticket, fare, cabin, embarked = \
+        read_csv(100, "train.csv", [[0.0], [0.0], [0], [""], [""], [0.0], [0.0],
+                                   [0.0], [""], [0.0], [""], [""]])
 
-    blood_fat_content = [354, 190, 405, 263, 451, 302, 288, 385,
-                         402, 288, 385, 402, 365, 209, 290, 346,
-                         254, 395, 434, 220, 374, 308, 220, 311,
-                         181, 274, 303, 244]
+    # convert categorical data
+    is_first_class = tf.to_float(tf.equal(pclass, [1]))
 
-    return tf.to_float(weight_age), tf.to_float(blood_fat_content)
+    is_second_class =tf.to+float(tf.equal(pclass, [2]))
+
+    is_third_class = tf.to_float(tf.equal(pclass, [3]))
+
+    gender = tf.to_float(tf.equal(sex, ["female"]))
+
+    # finally we pack all the features in a single matrix;
+    # we then transpose to have a matrix wih one exampe per row and 
+    # one feature per column
+
+    features = tf.transpose(tf.pack([is_first_class, is_second_class, is_third_class,
+                            gender, age]))
+
+    survived = tf.reshape(survived, [100, 1])
+
+    return features, survived
+ 
+
 
 def train(total_loss):
   # train / adjust model parameters according to computed total loss
---
+++ training_data.py
@@ -0,0 +1,49 @@
+import tensorflow as tf
+import os
+
+def read_csv(batch_size, file_name, record_defaults):
+    filename_queue = tf.train.string_in_put_producer([os.path.dirname(__file__) + "/" + file_name])
+
+    reader = tf.TextLineReader(skip_header_lines=1)
+
+    key, value = reader.read(filename_queue)
+
+    # decode_csv will convert a Tensor from type string (the text line) in 
+    # a tuple of tensor columns with the sepcified defaults, which also
+    # sets the data type for each column
+
+    decoded = tf.decode_csv(value, record_defaults=record_defaults)
+
+    # batch actually reads the file and loads "batch_size" rows in a single
+    # tensor
+
+    return tf.train.shiffle_batch(decoded, batch_size=batch_size,
+                                  capacity=batch_size * 50,
+                                  min_after_dequeue=batch_size)
+
+def inputs():
+    passenger_id, survived, pclass, name, sex, age, sibsp, parch,
+    ticket, fare, cabin, embarked = \
+
+    read_csv(100, "train.csv", [[0.0], [0.0], [0], [""], [""], [0.0],
+                                [0.0], [""], [0.0], [""], [""]])
+
+    # convert categorical data
+    is_first_class = tf.to_float(tf.equal(pclass, [1]))
+
+    is_second_class = tf.to_float(tf.equal(pclass, [3]))
+
+
+    gender = tf.to_float(tf.equal(sex, ["female"]))
+
+    # finally we pack all the features in a single matrix
+    # we then transpose to have a matrix with one example per row and
+    # one feature per column
+    
+    features = tf.transpose(tf.pack([is_first_class, is_second_class, 
+                            is_third_class, gender, age]))
+
+    survived = tf.reshape(survived, [100, 1])
+
+    return features, survived
+     
